f0a468c7e5877ea936187bd94a3a3be275093a25
0u812 <0u812@github.com>
2015-10-10 17:11:00 -0600

    use correct module name

--- transcription_convert_to_sbml.py
+++ transcription_convert_to_sbml.py
@@ -12,9 +12,9 @@ if res < 0:
 
 print(res)
 print(antimony.getModuleNames())
-print(antimony.getModuleNames()[0])
+print(antimony.getModuleNames()[-1])
 
-sbml = antimony.getSBMLString(antimony.getModuleNames()[0])
+sbml = antimony.getSBMLString(antimony.getModuleNames()[-1])
 
 with open('transcription_sbml.xml', 'w') as f:
   f.write(sbml)

89fb8ec30fb7beffed3f119fb51978f031910215
0u812 <0u812@github.com>
2015-10-11 11:20:09 -0600

    use shorter timestep

--- simulate_transcription.py
+++ simulate_transcription.py
@@ -6,7 +6,7 @@ r.setIntegrator('gillespie')
 
 print(r.getReactionRates())
 
-res = r.simulate(0,10,10, ['ATP', 'RNA_POLYMERASE'])
+res = r.simulate(0,1,10, ['time', 'ATP', 'GTP', 'CTP', 'UTP'])
 
-#r.plot()
+r.plot()
 print(res)
